ff884a4d30de3c16390814e29b4c5b11384579dc
viceblue <zhuhang0724@gmail.com>
2017-01-11 18:03:48 -0700

    1st lab

--- sc.py
+++ sc.py
@@ -4,7 +4,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/viceblue/404/master/sc.py")
 
 print response.text
 print response.status_code
